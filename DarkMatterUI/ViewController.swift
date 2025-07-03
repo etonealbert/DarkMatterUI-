e8f8c1f738c11681f48f63a882642485ec6fad6a
Albert Lukmanov <albert.lukmanov99@gmail.com>
2025-07-02 23:37:36 -0300

    Draw views by myself

--- DarkMatterUI/ViewController.swift
+++ DarkMatterUI/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        preparePlainView()
     }
 
+    func preparePlainView() {
+        let plainView = MyView(frame: CGRect(x: 100, y: 200, width: 100, height: 200 ))
+        
+        
+        view.addSubview(plainView)
+    }
 
 }
 
+
+
+class MyView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commontInit()
+    }
+    
+    required override init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commontInit()
+
+    }
+    
+    private func commontInit() {
+        backgroundColor = .yellow
+    }
+}
